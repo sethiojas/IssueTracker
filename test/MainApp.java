29b149a097240d3b34d23bf16acbb8dde03f19fa
Ojas Sethi <sethi.ojas@gmail.com>
2020-05-31 21:14:52 +0530

    moved code for login.fxml to LoginController.java

--- test/MainApp.java
+++ test/MainApp.java
@@ -5,43 +5,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.layout.*;
 import javafx.scene.*;
-import javafx.fxml.FXML;
-import javafx.scene.control.*;
-import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.stage.Window;
-
 
 public class MainApp extends Application {
-    private Stage stage;
-    @FXML
-    private TextField username;
-
-    @FXML
-    private TextField password;
-
-    @FXML
-    private Button loginButton;
-
-    @FXML
-    void checkCredentials(ActionEvent event){
-        String uname = username.getText();
-        String passwd = password.getText();
-
-        String res = CheckCreds.check(uname, passwd);
-        if (res == null){
-            Window owner = loginButton.getScene().getWindow();
-            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Wrong Credentials",
-                                    "Please check the entered Username and Password");
-        }
-
-        System.out.print("Success " + uname + " | " + passwd + " | " + res);
-    }
-    
-
+      
     @Override
     public void start(Stage primaryStage) throws Exception{
-        this.stage = primaryStage;
         GridPane root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("IssueTracker Login");
         primaryStage.setScene(new Scene(root));
