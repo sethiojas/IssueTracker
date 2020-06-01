b5f26cfc00049f5216b1c7b42c80c16d69d9e998
Ojas Sethi <sethi.ojas@gmail.com>
2020-06-01 13:13:49 +0530

    made methods public

--- src/com/issue_tracker/Bug.java
+++ src/com/issue_tracker/Bug.java
@@ -9,29 +9,29 @@ public class Bug implements Serializable{
     private boolean isClosed = false;
     final private int bugId;
 
-    Bug(String _title, String _desc, int _id){
+    public Bug(String _title, String _desc, int _id){
         title = _title;
         description = _desc;
         bugId = _id;
     }
 
-    String getTitle(){
+    public String getTitle(){
         return title;
     }
 
-    String getDescription(){
+    public String getDescription(){
         return description;
     }
 
-    int getId(){
+    public int getId(){
         return bugId;
     }
 
-    boolean isResolved(){
+    public boolean isResolved(){
         return isClosed;
     }
 
-    void resolved(){
+    public void resolved(){
         isClosed = true;
     }
 
