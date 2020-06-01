777c1251fe764372201e913f25a64f58c3b84a63
Ojas Sethi <sethi.ojas@gmail.com>
2020-06-01 13:38:33 +0530

    Initial Commit

---
+++ test/ProjectController.java
@@ -0,0 +1,39 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.geometry.Pos;
+import com.issue_tracker.*;
+
+
+public class ProjectController {
+    @FXML
+    private Label projectTitle;
+
+    @FXML
+    private VBox issueTitleVbox;
+
+    @FXML
+    private VBox maintainerVbox;
+
+    @FXML
+    private Button newBugButton;
+
+    public void initialize(Project proj){
+        projectTitle.setText(proj.getProjectName());
+
+        for (String uname: proj.getMaintainers()){
+            Label name = new Label(uname);
+            name.setMaxWidth(Double.MAX_VALUE);
+            maintainerVbox.getChildren().add(name);
+        }
+
+        for (Bug bug: proj.getAllBugs()){
+            Button btn = new Button(bug.getTitle());
+            btn.setMaxWidth(Double.MAX_VALUE);
+            btn.setAlignment(Pos.BASELINE_LEFT);
+            issueTitleVbox.getChildren().add(btn);
+        }
+    }
+}
