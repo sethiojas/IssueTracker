4051dfbcefe90e6a7becef992d99c70186c15377
Ojas Sethi <sethi.ojas@gmail.com>
2020-06-02 18:01:14 +0530

    Initial commit

---
+++ test/AddBugController.java
@@ -0,0 +1,48 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Button;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import com.issue_tracker.*;
+
+public class AddBugController {
+    private Project project;
+    private String contributorUName;
+
+    @FXML
+    private TextField bugTitle;
+
+    @FXML
+    private TextArea bugDescription;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    public void submit(ActionEvent event){
+        project.createNewBug(bugTitle.getText(), bugDescription.getText());
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("project.fxml"));
+            BorderPane root = loader.load();
+            ProjectController cont = loader.getController();
+            cont.initialize(project, contributorUName);
+            Stage stage = (Stage) submitButton.getScene().getWindow();
+            stage.setTitle("Project");
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void initialize(Project proj, String uname){
+        project = proj;
+        contributorUName = uname;
+    }
+}
