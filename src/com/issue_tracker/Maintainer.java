13fec3f4eb49cf0e6b3675b9920ceb05d1a7cef2
Ojas Sethi <sethi.ojas@gmail.com>
2020-06-05 18:23:10 +0530

    Use Contributor's db function Removed self db related functions. Added appropriate calls to super's functions

--- src/com/issue_tracker/Maintainer.java
+++ src/com/issue_tracker/Maintainer.java
@@ -1,12 +1,6 @@
 package com.issue_tracker;
 
-import java.util.HashMap;
 import java.io.Serializable;
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class Maintainer extends Contributor implements Serializable {
     private String manager;
@@ -16,97 +10,26 @@ public class Maintainer extends Contributor implements Serializable {
 
     public Maintainer(String _uName){
         uName = _uName;
-        saveMaintainer();
+        saveContributor();
     }
 
     public void addProject(String title, int id){
         projects.put(title, id);
-        updateMaintainer();
+        updateContributor();
     }
 
     public void removeProject(String title){
         projects.remove(title);
-        updateMaintainer();
+        updateContributor();
     }
 
     public void setManager(String uNameManager){
         manager = uNameManager;
-        updateMaintainer();
+        updateContributor();
     }
 
     public String getManager(){
         return manager;
-    }
-
-    public void saveMaintainer(){
-        String saveMaintainer = "INSERT INTO contributors VALUES(?, ?, ?)";
-        try{
-            Connection conn = DriverManager.getConnection(dbPath);
-            PreparedStatement pstm = conn.prepareStatement(saveMaintainer);
-            pstm.setString(1, uName);
-            pstm.setString(3, "true");
-            byte[] arr = ConvertObject.<Maintainer>getByteArrayObject(this);
-            if (arr == null) throw new Exception("Unable to Serialize object");
-            pstm.setBytes(2, arr);
-            pstm.executeUpdate();
-            conn.close();
-        }catch(SQLException e){
-            e.printStackTrace();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    public void updateMaintainer(){
-        String update = "UPDATE contributors SET object=? WHERE uname=?";
-        try{
-            Connection conn = DriverManager.getConnection(dbPath);
-            PreparedStatement pstm = conn.prepareStatement(update);
-            pstm.setString(2, uName);
-            byte[] arr = ConvertObject.<Maintainer>getByteArrayObject(this);
-            if (arr == null) throw new Exception("Unable to Serialize object");
-            pstm.setBytes(1, arr);
-            pstm.executeUpdate();
-            conn.close();
-        }catch(SQLException e){
-            e.printStackTrace();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    public static void removeMaintainer(String uname){
-        String delete = "DELETE FROM contributors WHERE uname=?";
-        try{
-            Connection conn = DriverManager.getConnection(dbPath);
-            PreparedStatement pstm = conn.prepareStatement(delete);
-            pstm.setString(1, uname);
-            pstm.executeUpdate();
-            conn.close();
-        }catch(SQLException e){
-            e.printStackTrace();
-        }
-    }
-
-    public static Maintainer getMaintainer(String uname){
-        String getMaintainer = "SELECT object FROM contributors WHERE uname=?";
-        Maintainer m = null;
-        try{
-            Connection conn = DriverManager.getConnection(dbPath);
-            PreparedStatement pstm = conn.prepareStatement(getMaintainer);
-            pstm.setString(1, uname);
-            ResultSet res = pstm.executeQuery();
-            byte[] arr = res.getBytes("object");
-            m = ConvertObject.<Maintainer>getJavaObject(arr);
-            if (m == null) throw new Exception("Unable to Deserialize object");
-            conn.close();
-        }catch(SQLException e){
-            e.printStackTrace();
-        }catch(Exception e){
-            e.printStackTrace();
-        }finally{
-            return m;
-        }
     }
 
     @Override
