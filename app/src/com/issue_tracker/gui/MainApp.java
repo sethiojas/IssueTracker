f45bcd370be805ad5c2028c117ac8664be499a5e
Ojas Sethi <sethi.ojas@gmail.com>
2020-06-06 14:23:56 +0530

    Changed fxml file path

--- app/src/com/issue_tracker/gui/MainApp.java
+++ app/src/com/issue_tracker/gui/MainApp.java
@@ -11,7 +11,7 @@ public class MainApp extends Application {
       
     @Override
     public void start(Stage primaryStage) throws Exception{
-        GridPane root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        GridPane root = FXMLLoader.load(getClass().getResource("../../fxml/login.fxml"));
         primaryStage.setTitle("IssueTracker Login");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
