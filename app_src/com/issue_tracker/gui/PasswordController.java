be46cf7fa4951149310aca448b5cff8e330d2a2e
Ojas Sethi <sethi.ojas@gmail.com>
2020-06-09 00:01:50 +0530

    Initial commit

---
+++ app_src/com/issue_tracker/gui/PasswordController.java
@@ -0,0 +1,55 @@
+package com.issue_tracker.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import com.issue_tracker.ChangePassword;
+
+/**This class handles change of password 
+*/
+
+public class PasswordController {
+    private String uname;
+
+    @FXML
+    private TextField oldPasswordField;
+
+    @FXML
+    private TextField newPasswordField;
+
+    @FXML
+    private TextField reEnterPasswordField;
+
+    @FXML
+    private Button okButton;
+
+    @FXML
+    public void okAction(){
+        String oldPassword = oldPasswordField.getText();
+        String newPassword = newPasswordField.getText();
+        String reEnterPassword = reEnterPasswordField.getText();
+
+        Stage stage = (Stage) okButton.getScene().getWindow();
+
+        if (oldPassword.equals(newPassword)){
+            AlertHelper.showAlert(Alert.AlertType.WARNING, stage, "Warning",
+                                 "Old password can not be same as New Password!");
+            return;
+        }else if (!newPassword.equals(reEnterPassword)){
+            AlertHelper.showAlert(Alert.AlertType.WARNING, stage, "Warning",
+                                 "Passwords do not match!");
+            return;
+        }
+        ChangePassword.change(uname, newPassword);
+        stage.close();
+    }
+
+    public void initialize(String uname){
+        /**Initialize the class variables
+        *@param uname       Uname of maintainer
+        */
+        this.uname = uname;
+    }
+}
